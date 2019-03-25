49f3b9c6ca82172d6290a4d55c5ff61c125702b7
Destiny <dshillis1@gmail.com>
2019-03-25 16:40:58 -0600

    object-ception

--- js/app.js
+++ js/app.js
@@ -200,6 +200,37 @@ console.log(obj);
 
 
 
+// 4. Object-ception
+// With the following object:
+
+const inception = {
+   reality: {
+       dreamLayer1: {
+           dreamLayer2: {
+               dreamLayer3: {
+                   dreamLayer4: {
+                       dreamLayer5: {
+                           dreamLayer6: {
+                               limbo: "Joseph Gordon Levitt"
+                          }
+                       }
+                   }
+               }
+           }
+       }
+   }
+}
+// Change the value of limbo to null.
+
+inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = null;
+
+const limbo = inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo;
+
+console.log(limbo);
+
+
+
+
 // function grossToNumber (str){
 //     //how can only numbers get in?
 // }
